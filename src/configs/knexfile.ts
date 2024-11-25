e2da836d2eb886462d1db131677dc7a908f7111d
unknown <henkyv60@gmail.com>
2024-11-25 16:29:28 +0700

    Part7integrasi-database-dengan-knex-migrasi-dan-seed

---
+++ src/configs/knexfile.ts
@@ -0,0 +1,27 @@
+import type { Knex } from 'knex';
+
+import configApp from '../utils/config';
+// Update with your config settings.
+
+const config: Knex.Config = {
+  client: configApp.KNEX_CLIENT,
+  connection: {
+    host: configApp.KNEX_CONNECTION_HOST,
+    port: configApp.KNEX_CONNECTION_PORT,
+    database: configApp.KNEX_CONNECTION_DATABASE,
+    user: configApp.KNEX_CONNECTION_USER,
+    password: configApp.KNEX_CONNECTION_PASSWORD,
+  },
+  pool: {
+    min: configApp.KNEX_POOL_MIN,
+    max: configApp.KNEX_POOL_MAX,
+  },
+  migrations: {
+    tableName: configApp.KNEX_MIGRATIONS_TABLENAME,
+    directory: './../migrations',
+  },
+  seeds: {
+    directory: './../seeds',
+  },
+};
+module.exports = config;
